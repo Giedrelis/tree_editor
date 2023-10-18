168a43d6707d5e0c4dc295cb97668d6b45674948
Giedrelis <58008054+Giedrelis@users.noreply.github.com>
2023-10-19 00:58:44 +0300

    lolol

--- decision_tree_editor.py
+++ decision_tree_editor.py
@@ -1,1 +1,27 @@
 
+import streamlit as st
+import streamlit.components.v1 as components
+
+# Load the custom component
+tree_editor = components.declare_component("tree_editor", url="http://localhost:3001")
+
+def main():
+    st.title("Graphical Tree Editor")
+
+    # Sample tree data (this will be dynamic in the final version)
+    tree_data = {
+        "name": "Root",
+        "children": [
+            {"name": "Child 1"},
+            {"name": "Child 2", "children": [{"name": "Grandchild"}]}
+        ]
+    }
+
+    # Render the tree editor component
+    updated_tree_data = tree_editor(tree_data=tree_data)
+
+    # Display the updated tree data (for debugging purposes)
+    st.json(updated_tree_data)
+
+if __name__ == "__main__":
+    main()
